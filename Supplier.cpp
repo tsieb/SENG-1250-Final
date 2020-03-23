95133b15a45067658c2a2d013c72ba92ce1a9b53
Noah-seng <noahmarv@gmail.com>
2020-03-22 19:43:14 -0700

    Supply functions defined.

--- Supplier.cpp
+++ Supplier.cpp
@@ -1,19 +1,78 @@
 #include "Supplier.h"
 #include "Hospital.h"
+#include <string>
 #include <fstream>
 using namespace std;
 
 Supplier::getSupplyList()
 {
-
+  supplies.open("Supplies.dat", ios::in | ios::binary);
+  while(getline(supplies, supply))
+  {
+    supplies>>id>>supply>>stock;
+    cout<<supply<<" "
+  }
 }
 
-Supplier::void depositSupplies()
+Supplier::depositSupplies()
 {
-
+  cout<<"Enter the ID of the supply you wish to deposit to, and the amount to deposit: ";
+  cin>>in>>amount;
+  supplies.open("Supplies.dat", ios::app | ios::binary);
+  while(getline(supplies, supply))
+  {
+    supplies>>id>>supply>>stock;
+    if(id==in)
+    {
+      cout<<id<<" "<<supply<<" "<<stock;
+      supplies.close;
+      break;
+    }
+    else
+    {
+      cout<<"There is no supply with this ID.";
+    }
+  }
 }
 
-Supplier::void returnStock()
+Supplier::withdrawSupplies()
 {
+  cout<<"Enter the ID of the supply you wish to withdraw from, and the amount to withdraw: ";
+  cin>>in>>amount;
+  supplies.open("Supplies.dat", ios::app | ios::binary);
+  while(getline(supplies, supply))
+  {
+    supplies>>id>>supply>>stock;
+    if(id==in)
+    {
+      cout<<id<<" "<<supply<<" "<<stock;
+      supplies.close;
+      break;
+    }
+    else
+    {
+      cout<<"There is no supply with this ID.";
+    }
+  }
+}
 
+Supplier::outputStock()
+{
+  cout<<"Enter the ID of the supply you wish to check the stock of: ";
+  cin>>in;
+  supplies.open("Supplies.dat", ios::in | ios::binary);
+  while(getline(supplies, supply))
+  {
+    supplies>>id>>supply>>stock;
+    if(id==in)
+    {
+      cout<<stock;
+      supplies.close;
+      break;
+    }
+    else
+    {
+      cout<<"There is no supply with this ID.";
+    }
+  }
 }
--- Supplier.h
+++ Supplier.h
@@ -1,11 +1,17 @@
 #include "Hospital.h"
 #include <fstream>
+#include <string>
 using namespace std;
 
 class Supplier : public Hospital
 {
   public:
+    fstream supplies;
+    int id, stock, in, amount;
+    std::string supply;
+
     void getSupplyList();
     void depositSupplies();
-    void returnStock();
+    void withdrawSupplies()
+    int returnStock();
 };
