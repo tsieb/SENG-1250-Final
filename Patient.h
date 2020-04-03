18b7682667bedac809fe25cf5c7cb7d22fd12ba7
ahanaahluwalia <62190462+ahanaahluwalia@users.noreply.github.com>
2020-04-02 19:56:22 -0700

    Update Patient.h

--- Patient.h
+++ Patient.h
@@ -8,9 +8,6 @@ using namespace std;
 
 class Patient : public Hospital
 {
-    std::string symptoms[10];
-    std::string illnesses[10];
-    std::string allergies[10];
     std::string name;
     int age;
     string blood_type;
@@ -24,9 +21,7 @@ class Patient : public Hospital
     double get_Weight();
     void set_Height(double);
     double get_Height();
-    void add_Symptom(string);
-    void add_Illness(string);
-    void add_Allergy(string);
+    
     bool check_In_Patient();
 
 };
